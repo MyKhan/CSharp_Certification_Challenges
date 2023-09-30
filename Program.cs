d9b62f6d470f5cdf8f0d8f94e8a96182ff5fb136
Meher Yar Khan <meheryarkhan90@hotmail.com>
2023-09-30 15:58:19 +0300

    Testing splitting string into character array and Joining

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿//To call the various Files
-Console.WriteLine("Under Construction");
+﻿string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+//string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
