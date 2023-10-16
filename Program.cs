e77e1c2193de112754147d7efcd56c31afcd08a1
Meher Yar Khan <meheryarkhan90@hotmail.com>
2023-10-16 23:17:33 +0300

    Testing Value and Reference Type Parameters

--- Program.cs
+++ Program.cs
@@ -1,18 +1,2 @@
 ﻿Console.WriteLine("All the challenges are in their own files");
 
-string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
-
-Console.WriteLine("Is it a palindrome?");
-foreach (string word in words) 
-{
-    Console.WriteLine($"{word}: {IsPalindrome(word)}");
-}
-
-bool IsPalindrome(string word) 
-{
-    string reversedWord = new string(word.Reverse().ToArray());
-
-    bool result = word == reversedWord ? true : false;
-
-    return result;
-}
